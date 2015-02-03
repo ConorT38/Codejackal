6364f9558ea68ebc11a733aa995043ff7b89c1c9
ConorT38 <conorthompson213@gmail.com>
2015-02-03 12:18:13 +0000

    Create myUsers.js
    This is the Javascript file for the signup page to validate everything.

---
+++ myUsers.js
@@ -0,0 +1,50 @@
+
+function userController($scope) {
+$scope.fName = '';
+$scope.lName = '';
+$scope.passw1 = '';
+$scope.passw2 = '';
+$scope.users = [
+{id:1, fName:'Niall',  lName:"Curran" },
+{id:2, fName:'Conor',   lName:"Thompson" },
+{id:3, fName:'Seamus',   lName:"Fanning" },
+{id:4, fName:'Jelicho',  lName:"Bachin" }
+];
+$scope.edit = true;
+$scope.error = false;
+$scope.incomplete = false; 
+
+$scope.editUser = function(id) {
+  if (id == 'new') {
+    $scope.edit = true;
+    $scope.incomplete = true;
+    $scope.fName = '';
+    $scope.lName = '';
+    } else {
+    $scope.edit = false;
+    $scope.fName = $scope.users[id-1].fName;
+    $scope.lName = $scope.users[id-1].lName; 
+  }
+};
+
+$scope.$watch('passw1',function() {$scope.test();});
+$scope.$watch('passw2',function() {$scope.test();});
+$scope.$watch('fName', function() {$scope.test();});
+$scope.$watch('lName', function() {$scope.test();});
+$scope.$watch('email', function() {$scope.test();});
+
+$scope.test = function() {
+  if ($scope.passw1 !== $scope.passw2) {
+    $scope.error = true;
+    } else {
+    $scope.error = false;
+  }
+  $scope.incomplete = false;
+  if ($scope.edit && (!$scope.fName.length ||
+  !$scope.lName.length ||
+  !$scope.passw1.length || !$scope.passw2.length)) {
+       $scope.incomplete = true;
+  }
+};
+
+}
