df29ade946f2cd4aaac61699e397e7f6788f3d68
ConorT38 <conorthompson213@gmail.com>
2015-02-03 16:20:30 +0000

    Update index.php

--- index.php
+++ index.php
@@ -117,9 +117,9 @@ $('#clickme').on('click', function() {
           <p><a href="tutorial.php">more...</a></p></div>
         <div class="col-md-3"> 
           <h2>Twitter Feed</h2>
-		  	<a class="twitter-timeline" href="https://twitter.com/CodeJackals" data-widget-id="561112170742968320">Tweets by @CodeJackals</a>
+          <!--Changed the motherfucking Twitter Feed pointer -->
+<a class="twitter-timeline" href="https://twitter.com/CptCodeJackal" data-widget-id="562646270217814016">Tweets by @CptCodeJackal</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-   
         </div>
      <div class="col-md-3">
          <h2 >Facebook Feed</h2>
