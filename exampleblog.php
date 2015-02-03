b1f3595c0ea49bfddbd50c3ea641725eefa5d473
ConorT38 <conorthompson213@gmail.com>
2015-02-03 11:45:16 +0000

    Create exampleblog.php
    This is an example blog api page, this is done.

---
+++ exampleblog.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CodeJackal | Example Blog</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootswatch/3.3.2/superhero/bootstrap.min.css">
+<link rel="stylesheet" src="socialbtn.css" type="text/css"/>
+
+<script src= "http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+ <link async href="http://fonts.googleapis.com/css?family=Warnes" data-generated="http://enjoycss.com" rel="stylesheet" type="text/css"/>
+<script>
+  $(function () {
+  $('[data-toggle="popover"]').popover()
+})
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+  })
+  $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+  </script>
+
+	<link rel="shortcut icon" href="food.ico">
+  </head>
+
+  <body data-spy="scroll" data-target="#myScrollspy">
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+          <a class="navbar-brand" href="index.php">CodeJackal</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home</a></li>
+             <li><a href="list.php">Archive</a></li>
+            <li><a href="random.php">Random</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li data-toggle="tooltip" data-placement="bottom" title="This is the most highly rated tutorial post"><a href="leaderboard.php">Code of the Month!</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="Signup.php" data-toggle="tooltip" data-placement="bottom" title="This is not quite ready yet, sozzles." ><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="Login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </div>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div id="na">
+    <div class="row">
+        <div class="col-xs-3" id="myScrollspy">
+            <ul class="nav nav-tabs nav-stacked" data-spy="affix" data-offset-top="125">
+                <li><a href="#question1">Question 1</a></li>
+                <li><a href="#question2">Question 2</a></li>
+                <li><a href="#question3">Question 3</a></li>
+            </ul>
+        </div>
+      </div>
+    </div>
+    <style>
+#na{
+  position:fixed;
+}
+
+    </style>
+    <div id = "alert_placeholder"></div>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="myModalLabel">A little helping hand...</h4>
+      </div>
+      <div class="modal-body">
+        So you've made it to the Login part of the site, good for you, you're brilliant. That means you made a page, and are trying to Login to that page, so you can get started That's good. I like you.<br>
+        Judging by you clicking on this, you need help. That's no problem. Just means you're not a techy perhaps.<hr>
+        <br>
+        What you're gonna want to do, is just enter in all of your silly little credentials (Email, Password) and I'll let the server do the rest.<br>
+        Trust me, it's easy!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div id = "alert_placeholder"></div>
+    <div class="container">
+      <div class="jumbotron">
+        <h1>Example blog:</h1>      
+    <p>The blogging system will look like this...</p>
+      
+      </div>
+
+    </div>
+
+    <div class="container">
+      <div class="well">
+
+        <h4>This is the fuckin' heading</h4>
+        <br>
+        <br>
+
+Mel ad tempor inermis necessitatibus, eum ex case summo invidunt. Id pro ferri dolore dissentias, hinc philosophia an mea. In nonumes oportere dignissim eum, vim magna scaevola no. Solet putent ex sed, ne mazim accusam vix, ut quo verterem posidonium. Nibh legimus detraxit sit ex, sanctus accumsan pri an. Per latine quaeque albucius ei, feugait adipiscing definiebas has eu.
+
+Ne ludus sanctus fastidii vis, vim nominavi antiopam ea. Vim ut quem nonumes epicuri, pro elit discere adipisci id. Nam et alia tincidunt adipiscing, eos ea commodo pertinax platonem. Stet modus ipsum ex has, no sea zril ignota delectus. Purto putent duo ne, et quod dolor usu. Indoctum tincidunt pertinacia id eum.
+
+Alii possim mandamus vel eu, alterum salutandi dignissim no mea. Eu erant verterem instructior pro, ad eos sanctus civibus. Nam eu tota novum corpora, eu sit amet sonet, at vix error veniam platonem. Est oporteat eloquentiam eu. Vix clita noster explicari cu.
+
+Evertitur dissentiet no ius.Quo duis simul nemore ex. No solum volutpat eam. Vim cu postea vituperata dissentiet, cetero audire diceret ea vel.
+
+Error appellantur vis id. Latine bonorum inermis ea eam, sea in consul melius meliore. Vis elit affert id, ex pro meliore epicurei. Quo cu dolorem forensibus, his ei duis nullam tritani. Ullum primis inermis cum ne, brute sonet elitr mel ut.
+
+Stet omnes deterruisset eam an, ei est mundi choro habemus. Expetenda forensibus vis eu, ea vis congue libris commodo, adversarium concludaturque duo in. Reprimique omittantur quo eu, lorem omnium at eum, his oporteat consequuntur cu. Has vero saperet delicatissimi ad, ad partiendo maiestatis has, vim at tota docendi. Debet solet delenit nec ei, nam mutat justo ei. Eu usu alienum tibique fabellas, ludus atomorum vim id.
+
+Qui option hendrerit eu, ius id eligendi assentior expetendis. Te quo vivendum posidonium, no sea facilis rationibus, pertinax praesent sea et. Deserunt intellegat persequeris mea eu, duis senserit sententiae ex nam. Vix ei veri conceptam interesset, an habemus molestie quo. Eu paulo eirmod persecuti est. Vel eu utamur similique, ius tibique corrumpit tincidunt in. Mel nobis denique temporibus te.
+
+Sumo mucius qualisque eu usu, eam cu graeco tamquam. Eos malorum insolens te, regione meliore definitiones in vel. Mei ex vide nullam pericula, affert dissentias in pri. Sea no vitae adipisci, mei veniam regione te.
+
+At tibique salutandi quaerendum duo, in nominavi reprimique eos, eu sint duis detracto nec. At munere discere vocibus eos. Et has vidit libris elaboraret. Mea audiam discere dolorum et, an quo menandri mnesarchum. Per case accusam definiebas ad, pri eripuit patrioque aliquando te. Cu nisl tale mandamus vel, mei ea movet appareat theophrastus, sed te esse oblique salutatus. Mea malorum mentitum interpretaris cu, ne vel posse incorrupte.
+
+Molestiae assentior eu pri, ne paulo fabellas quo. Zril quidam tacimates pri ad, in vidisse quaestio tincidunt vix, sit tollit euripidis aliquando et. Te pri postea vulputate, posse iusto delectus id his, illud neglegentur mea et. Ei est rebum conclusionemque, cum an tritani conclusionemque, nec nostrum vivendum assentior et. Laudem praesent inciderint pri ne.
+  <img src="example.png" data-toggle="tooltip" data-placement="bottom" title="This is an example of a responsive image inset of the blog." class="img-responsive" width="400" height="200"/>     
+      </div>
+
+<div class="container">
+      <div class="well">
+<h3 id="question1"><u>Question 1: </u></h3><p> <i>What did I mean in the first example?</i></p>
+<br>
+<p>A: Nothing</p>
+<p>B: That you were a dork.</p>
+<p>C: That this was the obvious answer</p>
+<br>
+<div class="btn-group" data-toggle="buttons">
+  <label class="btn btn-primary active">
+    <input type="radio" name="options" id="option1" autocomplete="off"> A
+  </label>
+  <label class="btn btn-primary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> B
+  </label>
+  <label class="btn btn-primary">
+    <input type="radio" name="options" id="option3" autocomplete="off"> C
+  </label>
+</div>
+
+</div>
+</div>
+
+<div class="container">
+      <div class="well">
+<h3 id="question2"><u>Question 2: </u></h3><p> <i>What did I mean in the first example?</i></p>
+<br>
+<p>A: Nothing</p>
+<p>B: That you were a dork.</p>
+<p>C: That this was the obvious answer</p>
+<br>
+<div class="btn-group" data-toggle="buttons">
+  <label class="btn btn-primary active">
+    <input type="radio" name="options" id="option1" autocomplete="off"> A
+  </label>
+  <label class="btn btn-primary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> B
+  </label>
+  <label class="btn btn-primary">
+    <input type="radio" name="options" id="option3" autocomplete="off"> C
+  </label>
+</div>
+
+</div>
+</div>
+
+<div class="container">
+      <div class="well">
+<h3 id="question3"><u>Question 3: </u></h3><p> <i>What did I mean in the first example?</i></p>
+<br>
+<p>A: Nothing</p>
+<p>B: That you were a dork.</p>
+<p>C: That this was the obvious answer</p>
+<br>
+<div class="btn-group" data-toggle="buttons">
+  <label class="btn btn-primary active">
+    <input type="radio" name="options" id="option1" autocomplete="off"> A
+  </label>
+  <label class="btn btn-primary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> B
+  </label>
+  <label class="btn btn-primary">
+    <input type="radio" name="options" id="option3" autocomplete="off"> C
+  </label>
+</div>
+
+</div>
+</div>
+
+
+
+      <div ng-app="" ng-controller="myController">
+      <button type="button" class="btn btn-default" ng-click="count = count + 1" >
+          <span class="glyphicon glyphicon-chevron-up"></span></button><span>
+        <button type="button" class="btn btn-default" ng-click="count = count - 1">
+          <span class="glyphicon glyphicon-chevron-down"></span></button>&nbsp;&nbsp;<p>Score: {{ count }} </p></span>
+        </div>
+      <div>
+        <script>
+function myController($scope) {
+    $scope.count = 0;
+}
+</script>
+      <div class="clearfix visible-lg"></div>
+	  <center>
+	  <footer class ="footer">
+	  <div class = "container">
+	  <p class=" text-muted">Code Jackal &copy; 2015</p>
+	  </div>
+	  </footer>
+	  </center>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
