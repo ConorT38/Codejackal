7acb28387ea05b054212ed3dda7375a4f2ba4c63
Maroxide <seamusfanning@hotmail.co.uk>
2015-02-14 15:08:14 +0000

    Create tables.php
    Okay so Conor when we were making them in php my admin we were basically just doing this.

---
+++ tables.php
@@ -0,0 +1,15 @@
+<? PHP
+<!-- this means we dont have to type out the sql details every time we want to link something by the way _ Seamus just experimenting here -->
+
+include mysql.php;
+
+<!-- this is how you make a table in php for an sql database_Seamus,I researched this for ages -->
+
+$table = "ENTRIES";
+mysql_query ("CREATE TABLE IF NONE EXISTS '$table' ('ID' INT NOT NULL AUTO_INCREMENT , PRIMARY KEY ('id')) ");
+mysql_query ("ALTER TABLE '$table' ADD 'TITLE' TEXT NOT NULL");
+mysql_query ("ALTER TABLE '$table' ADD 'CONTENT' TEXT NOT NULL");
+mysql_query ("ALTER TABLE '$table' ADD 'VOTES' varchar(255) NOT NULL");
+
+
+?>
