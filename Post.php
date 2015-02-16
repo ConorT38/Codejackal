51a47ca879f946030446a2e0d19c9c5751443ffe
Conor Thompson <conorthompson213@gmail.com>
2015-02-16 21:36:48 +0000

    redirect added

--- Post.php
+++ Post.php
@@ -53,7 +53,7 @@ session_start();
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li class="active"><a href="Post.php" data-toggle="tooltip" data-placement="bottom" title="Post a new blog" ><span class="glyphicon glyphicon-user"></span>Post</a></li>
-            <li><a href="Logout.php"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
+            <li><a href="redirect.php"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
           </div>
           </ul>
         </div>
