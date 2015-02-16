77cc17385a71e341997cec0e261866ccc96e51b4
Conor Thompson <conorthompson213@gmail.com>
2015-02-16 16:04:42 +0000

    Update Signup.php

--- Signup.php
+++ Signup.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootswatch/3.3.2/superhero/bootstrap.min.css">
+
+<!-- Latest compiled Angular.js library-->
 <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
 
 <!-- jQuery library -->
