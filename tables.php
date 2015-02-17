3493e371386552409596df4ac94e4f698e8593c7
Maroxide <seamusfanning@hotmail.co.uk>
2015-02-17 11:30:37 +0000

    Update tables.php

--- tables.php
+++ tables.php
@@ -1,9 +1,9 @@
 <? PHP
-<!-- this means we dont have to type out the sql details every time we want to link something by the way _ Seamus just experimenting here -->
+#this means we dont have to type out the sql details every time we want to link something by the way _ Seamus just experimenting here 
 
 include mysql.php;
 
-<!-- this is how you make a table in php for an sql database_Seamus,I researched this for ages -->
+#this is how you make a table in php for an sql database_Seamus,I researched this for ages
 
 $table = "ENTRIES";
 mysql_query ("CREATE TABLE IF NONE EXISTS '$table' ('ID' INT NOT NULL AUTO_INCREMENT , PRIMARY KEY ('id')) ");
