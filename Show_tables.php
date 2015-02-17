6bec403d216ab0a6888356fa11a3774536f11ff8
Conor Thompson <conorthompson213@gmail.com>
2015-02-17 11:11:20 +0000

    Create Show_tables.php

---
+++ Show_tables.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CodeJackal | Tables</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootswatch/3.3.2/superhero/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="stylesheet" src="socialbtn.css" type="text/css"/>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+ <link async href="http://fonts.googleapis.com/css?family=Warnes" data-generated="http://enjoycss.com" rel="stylesheet" type="text/css"/>
+<script>
+  $(function () {
+  $('[data-toggle="popover"]').popover()
+})
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+  })
+  $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+  </script>
+
+	<link rel="shortcut icon" href="food.ico">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+          <a class="navbar-brand" href="index.php">CodeJackal</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home</a></li>
+             <li><a href="list.php">Archive</a></li>
+            <li><a href="random.php">Random</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li data-toggle="tooltip" data-placement="bottom" title="This is the most highly rated tutorial post"><a href="leaderboard.php">Code of the Month!</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="Signup.php" data-toggle="tooltip" data-placement="bottom" title="This is not quite ready yet, sozzles." ><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="Login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </div>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div id = "alert_placeholder"></div>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="myModalLabel">A little helping hand...</h4>
+      </div>
+      <div class="modal-body">
+        So you've made it to the Login part of the site, good for you, you're brilliant. That means you made a page, and are trying to Login to that page, so you can get started That's good. I like you.<br>
+        Judging by you clicking on this, you need help. That's no problem. Just means you're not a techy perhaps.<hr>
+        <br>
+        What you're gonna want to do, is just enter in all of your silly little credentials (Email, Password) and I'll let the server do the rest.<br>
+        Trust me, it's easy!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div id = "alert_placeholder"></div>
+    <div class="container">
+      <div class="jumbotron">
+        <h1>All of the users within the database</h1>      
+         <br>
+
+         <!-- The following will be a table done in PHP and a simple for loop with all of the score listing of users and which score is the highest. The estimate
+         loop will be something like "for(int i=0; i<list.size(); i++){
+                                          echo $_POST['score']}; etc..-->
+    <?php
+$dbhost = 'localhost';
+$dbuser = 'wearefif_codejac';
+$dbpass = 'codejackalsql';
+$dbname = 'wearefif_codejackal';
+// Create connection
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+// Check connection
+if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT*FROM ENTRIES";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+     echo "<table><tr><th>Blog title</th><th>Content</th><th>Score</th></tr>";
+     // output data of each row
+     while($row = $result->fetch_assoc()) {
+         echo "<tr><td>" . $row["TITLE"]. " " . $row["CONTENT"]. "</td><td>" . $row["VOTES"]. "</td><td></tr>";
+     }
+     echo "</table>";
+} else {
+     echo "0 results";
+}
+$conn->close();
+?>  
+
+    </div>
+</div>
+      <div class="clearfix visible-lg"></div>
+	  <center>
+	  <footer class ="footer">
+	  <div class = "container">
+	  <p class=" text-muted">Code Jackal &copy; 2015</p>
+	  </div>
+	  </footer>
+	  </center>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
