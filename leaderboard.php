97e7bef79c1a67c26eb7975ae00400732882b48e
Conor Thompson <conorthompson213@gmail.com>
2015-02-19 10:43:26 +0000

    Update leaderboard.php

--- leaderboard.php
+++ leaderboard.php
@@ -50,6 +50,21 @@
             <li class="active" data-toggle="tooltip" data-placement="bottom" title="This is the most highly rated tutorial post"><a href="leaderboard.php">Code of the Month!</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
+          <li>
+	
+          <form class="navbar-form" role="search">
+		<div class="input-group">
+			<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-
+
+term">
+			<div class="input-group-btn">
+				<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-
+
+search"></i></button>
+			</div>
+		</div>
+		</form>
+		</li>
             <li><a href="Signup.php" data-toggle="tooltip" data-placement="bottom" title="This is not quite ready yet, sozzles." ><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
             <li><a href="Login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
           </div>
