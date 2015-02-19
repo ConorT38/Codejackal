9a56af77c89718474ea5a112b4f36968174e5601
Conor Thompson <conorthompson213@gmail.com>
2015-02-19 10:38:47 +0000

    Update index.php

--- index.php
+++ index.php
@@ -62,11 +62,21 @@ $('#clickme').on('click', function() {
           <ul class="nav navbar-nav navbar-right">
           <li>
 	
-            <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
-            <div class="input-group-btn">
-                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-            </div>
-        
+           <li>
+	
+          <form class="navbar-form" role="search">
+		<div class="input-group">
+			<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-
+
+term">
+			<div class="input-group-btn">
+				<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-
+
+search"></i></button>
+			</div>
+		</div>
+		</form>
+		</li>
 		</li>
             <li><a href="Signup.php" data-toggle="tooltip" data-placement="bottom" title="This is not quite ready yet, sozzles." ><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
             <li><a href="Login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
