87802d456e9a06a410d1543007b1ec91f53d1878
Maroxide <seamusfanning@hotmail.co.uk>
2015-02-20 10:46:35 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,16 @@
 <?php
+$servername = "localhost";
+$username = "codejackal_admin";
+$password = "Waltherp99";
+$dbname = "codejackal_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 
-
-
-
-
-?>
+$conn->close();
+?> 
