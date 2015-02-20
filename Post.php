66a21528d0069575e78ed95d60271ae3b16bdf87
Conor Thompson <conorthompson213@gmail.com>
2015-02-20 15:46:34 +0000

    Update Post.php

--- Post.php
+++ Post.php
@@ -40,6 +40,31 @@ else
 {
 ?>
 
+<!--Session data, sorry if it's messy -->
+<?php
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+$dbhost = 'localhost';
+$dbuser = 'codejackal_admin';
+$dbpass = 'Waltherp99';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+
+// Selecting Database
+$db = mysql_select_db("codejackal_database", $conn);
+
+session_start();// Starting Session This is important to put this in the right position faggots
+
+// Storing Session
+$user_check=$_SESSION['login_user'];
+
+// SQL Query To Fetch Complete Information Of User
+$ses_sql=mysql_query("select email from users where email='$user_check'", $connection);
+$row = mysql_fetch_assoc($ses_sql);
+$login_session =$row['email'];
+if(!isset($login_session)){
+mysql_close($connection); // Closing Connection
+header('Location: Logout.php'); // Redirecting To Home Page
+}
+?>
 
 <html>
   <head>
