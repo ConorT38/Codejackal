ca091ef4c85b6ed18fc21ac7ddfff069c8e50de6
Conor Thompson <conorthompson213@gmail.com>
2015-02-20 15:38:52 +0000

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,4 +1,45 @@
 <!DOCTYPE html>
+<?php
+session_start(); // Starting Session
+$error=''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+if (empty($_POST['username']) || empty($_POST['password'])) {
+$error = "Username or Password is invalid";
+}
+else
+{
+// Define $email and $pass
+$email=$_POST['email'];
+$pass=$_POST['pass'];
+
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+$dbhost = 'localhost';
+$dbuser = 'codejackal_admin';
+$dbpass = 'Waltherp99';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+
+// To protect MySQL injection for Security purpose
+$email = stripslashes($email);
+$pass = stripslashes($pass);
+$email = mysql_real_escape_string($email);
+$pass= mysql_real_escape_string($pass);
+
+// Selecting Database, make sure to change this to user if you mix it up
+$db = mysql_select_db("codejackal_database", $conn);
+
+// SQL query to fetch information of registerd users and finds user match.
+$query = mysql_query("select * from login where pass='$pass' AND email='$email'", $conn);
+$rows = mysql_num_rows($query);
+if ($rows == 1) {
+$_SESSION['login_user']=$email; // Initializing Session
+header("location: User.php"); // Redirecting To Other Page
+} else {
+$error = "Username or Password is invalid";
+}
+mysql_close($connection); // Closing Connection
+}
+}
+?>
 <html>
   <head>
     <title>CodeJackal | Login</title>
@@ -114,6 +155,7 @@ myForm.email.$dirty && myForm.email.$invalid" id = "clickme" value="Submit!"/>
         <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal">
   Does someone need help?
 </button>
+<small><span style="color:orange"><?php echo $error; ?>
    </div>
 <script>
 bootstrap_alert = function() {}
