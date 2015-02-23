a86e81ecb99d0e6a4b908610ca75797aa70c7cf6
Conor Thompson <conorthompson213@gmail.com>
2015-02-23 09:58:02 +0000

    Update Post.php

--- Post.php
+++ Post.php
@@ -105,7 +105,7 @@ header('Location: Logout'); // Redirecting To Home Page
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>                        
           </button>
-          <a class="navbar-brand" href="index.php">CodeJackal</a>
+          <a class="navbar-brand" href="index">CodeJackal</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
           <ul class="nav navbar-nav">
