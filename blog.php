08bf0b94cdbc76bc33702f1656da9b9918903a37
Conor Thompson <conorthompson213@gmail.com>
2015-02-26 11:53:51 +0000

    Create blog.php

---
+++ blog.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CodeJackal | Tables</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootswatch/3.3.2/superhero/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="stylesheet" src="socialbtn.css" type="text/css"/>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+ <link async href="http://fonts.googleapis.com/css?family=Warnes" data-generated="http://enjoycss.com" rel="stylesheet" type="text/css"/>
+<script>
+  $(function () {
+  $('[data-toggle="popover"]').popover()
+})
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').focus()
+  })
+  $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+  </script>
+
+	<link rel="shortcut icon" href="food.ico">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>                        
+          </button>
+          <a class="navbar-brand" href="index.php">CodeJackal</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home</a></li>
+             <li><a href="list.php">Archive</a></li>
+            <li><a href="random.php">Random</a></li>
+            <li><a href="about.html">About Us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li data-toggle="tooltip" data-placement="bottom" title="This is the most highly rated tutorial post"><a href="leaderboard.php">Code of the Month!</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+          <li>
+	
+          <form class="navbar-form" role="search">
+		<div class="input-group">
+			<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-
+
+term">
+			<div class="input-group-btn">
+				<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-
+
+search"></i></button>
+			</div>
+		</div>
+		</form>
+		</li>
+            <li><a href="Signup.php" data-toggle="tooltip" data-placement="bottom" title="This is not quite ready yet, sozzles." ><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <li><a href="Login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+          </div>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div id = "alert_placeholder"></div>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="myModalLabel">A little helping hand...</h4>
+      </div>
+      <div class="modal-body">
+        So you've made it to the Login part of the site, good for you, you're brilliant. That means you made a page, and are trying to Login to that page, so you can get started That's good. I like you.<br>
+        Judging by you clicking on this, you need help. That's no problem. Just means you're not a techy perhaps.<hr>
+        <br>
+        What you're gonna want to do, is just enter in all of your silly little credentials (Email, Password) and I'll let the server do the rest.<br>
+        Trust me, it's easy!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div id = "alert_placeholder"></div>
+    <div class="container">
+      <div class="jumbotron">
+        <h1><?php echo $title; ?></h1>      
+         <br>
+
+    </div>
+</div>
+ <div class="container">
+      <div class="jumbotron">
+        <p><i><?php echo $description;?></i></p>     
+         <br>
+         <br>
+         <br>
+         <br>
+        <p><?php echo $content;?>></p>     
+         <br>
+
+    </div>
+</div>
+      <div class="clearfix visible-lg"></div>
+	  <center>
+	  <footer class ="footer">
+	  <div class = "container">
+	  <p class=" text-muted">Code Jackal &copy; 2015</p>
+	  </div>
+	  </footer>
+	  </center>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
