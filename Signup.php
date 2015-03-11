c922a9c99572e8056bf04c6a7a9903dc64a044d0
Conor Thompson <conorthompson213@gmail.com>
2015-03-11 16:01:17 +0000

    Update Signup.php

--- Signup.php
+++ Signup.php
@@ -77,7 +77,7 @@ if (isset($_REQUEST['submitted'])) {
 }
 }
 }
-header('Location: signupsuccess.php');
+echo "<script> window.location.replace('signupsuccess.php') </script>" 
 ?>
 <!DOCTYPE html>
 <html>
