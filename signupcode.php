de8046de468ce872088181067abb734204767fa8
Conor Thompson <conorthompson213@gmail.com>
2015-03-12 12:30:27 +0000

    Create signupcode.php

---
+++ signupcode.php
@@ -0,0 +1,46 @@
+ <?php
+           while(isset($_POST['clickme'])){
+           if(!isset($_POST['fname'])&&($_POST['lname'])&&($_POST['email'])&&($_POST['pass'])){
+           	header("Location: Signup.php");
+           }
+           else{
+           $dbhost = 'localhost';
+$dbuser = 'codejackal_admin';
+$dbpass = 'Waltherp99';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}
+           
+if(isset($_POST['clickme']))
+
+{
+
+
+if(! get_magic_quotes_gpc() )
+{
+   $fname = addslashes ($_POST['fname']);
+   $lname = addslashes ($_POST['lname']);
+   $email = addslashes($_POST['email']);
+   $pass = addslashes($_POST['pass']);
+}
+else
+{
+   $fname = $_POST['fname'];
+   $lname = $_POST['lname'];
+   $email = $_POST['email'];
+   $pass= $_POST['pass'];
+}
+
+$sql = "INSERT INTO users ".
+       "(fname,lname,email,pass,) ".
+       "VALUES('$fname','$lname','$email','$pass')";
+mysql_select_db('codejackal_database');
+$retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Could not enter data: ' . mysql_error());
+}
+header('Location: Login.php');
+?>
