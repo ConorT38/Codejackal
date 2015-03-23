8119e3a3078ee84b34b8bd07add9551f862b3c15
Conor Thompson <conorthompson213@gmail.com>
2015-03-23 13:07:31 +0000

    Update User.php

--- User.php
+++ User.php
@@ -31,6 +31,18 @@ header("location:Login");
   $('[data-toggle="tooltip"]').tooltip()
 })
   </script>
+  <script>
+      $(document).ready(function() {
+          $("a.dropdown-toggle").click(function(ev) {
+              $("a.dropdown-toggle").dropdown("toggle");
+              return false;
+          });
+          $("ul.dropdown-menu a").click(function(ev) {
+              $("a.dropdown-toggle").dropdown("toggle");
+              return false;
+          });
+      });
+    </script>
 
 	<link rel="shortcut icon" href="food.ico">
   </head>
@@ -44,17 +56,17 @@ header("location:Login");
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>                        
           </button>
-          <a class="navbar-brand" href="User">CodeJackal</a>
+         <?php echo'<a class="navbar-brand" href="User.php?id='.$_SESSION["id"].'">CodeJackal</a>'?>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="User">Home</a></li>
-             <li><a href="Userlist">My Posts</a></li>
+            <?php echo '<li class="active"><a href="User.php?id='.$_SESSION["id"].'">Home</a></li>'; ?>  
+           <?php echo '<li><a href="Userlist.php?id=' .$_SESSION["id"].'">My Posts</a></li>'; ?>  
           </ul>          
             <ul class="nav navbar-nav navbar-right">
             	
        <li>
-          <form class="navbar-form" action="Usersearch.php?go" method="post" role="search">
+          <?php echo '<form class="navbar-form" action="Usersearch.php?go" method="post" role="search">';?>
 		<div class="input-group">
 			<input type="text" class="form-control" placeholder="Search" name="srchterm" id="srchterm">
 			<div class="input-group-btn">
@@ -64,7 +76,16 @@ header("location:Login");
 		</div>
 		</form>
 		</li>
-            <li><a href="Post" data-toggle="tooltip" data-placement="bottom" title="Post a new blog" ><span class="glyphicon glyphicon-pencil"></span>Post</a></li>
+ <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">My Info <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+           <?php echo '<li>Name: <small>'.$_SESSION['fname'].' '.$_SESSION['lname'].'</small></li>'; ?>
+             <?php echo '<li>Email: <small>'.$_SESSION['email'].'</small></li>'; ?>
+            <li class="divider"></li>
+            <?php echo '<li>Points:<small> '.$_SESSION['points'].'</small></li>'; ?>
+          </ul>
+        </li>
+            <?php echo '<li><a href="Post.php?id='.$_SESSION["id"].'" data-toggle="tooltip" data-placement="bottom" title="Post a new blog" ><span class="glyphicon glyphicon-pencil"></span>Post</a></li>';?>
             <li><a href="redirect"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
           </div>
           </ul>
@@ -94,8 +115,41 @@ header("location:Login");
         <h1>Welcome!</h1>
          <br>
          <p>Hey <?php echo $_SESSION['fname'];?>, ready to make a tutorial?</p>
-         
-         <h3>Why don't you check out some tutorials  <button type="button" class="btn btn-info" href="Userlist">Here</button>
+   
+         <h3>Why don't you check out some new tutorials:</h3>
+         </div>
+         </div>
+            <div class="container">
+    <?php
+ $dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
+$dbname = "codejackal_database"; // the name of the database that you are going to use for this project
+$dbuser = "codejackal_admin"; // the username that you created, or were given, to access your database
+$dbpass = "Waltherp99"; // the password that you created, or were given, to access your database
+ 
+mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
+mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
+    if(!isset($_GET['by'])){
+ $sql = "SELECT *FROM blog LIMIT 0, 10";
+$result = mysql_query($sql);
+while($row=mysql_fetch_array($result)){
+
+$title =$row['title'];
+	$description=$row['description'];
+	$id=$row['postID'];
+	$reg=$row['reg_date'];
+	$points =$row['points'];
+		
+//-display the result of the array
+echo '</div></div>';
+echo '<div class="container">';
+echo '<div class="jumbotron">';
+echo '<h3><u>Title:</u> <a href="Userblog.php?id='.$id.'">'  .$title .'</a></h3><br><small>Date: <i>' .$reg. '</i></small>';
+echo "<h4><u>Description: </u><i>" .$description. "</i></h4><br><h5><b>Points: ".$points. "</b></h5>";
+echo "</div>";
+echo "</div>";
+}
+}
+?>
 <hr>
 </div>
 </div>
