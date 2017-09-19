16af5f7e494eaef7825f5c2aa4401913e5b783ef
Conor Thompson <conorthompson213@gmail.com>
2017-09-19 14:19:23 +0100

    Update dbconnect.php

--- src/dbconnect.php
+++ src/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "codejackal_admin";
-$password = "Waltherp99";
-$dbname = "codejackal_database";
+$username = "";
+$password = "";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
